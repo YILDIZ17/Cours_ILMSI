de232e328271047bad8f6a2ed1dbe377c1b5e893
Tumay YILDIZ <yildiztumay94@gmail.com>
2026-04-27 14:44:39 +0200

    Ground and Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,6 +22,15 @@ public class Player : MonoBehaviour
     private Slider HPSlider;
 
     [SerializeField]
+    private float JumpForce = 7f;
+
+    [SerializeField]
+    private LayerMask GroundLayers;
+
+    [SerializeField]
+    private float GroundCheckDistance = 0.6f;
+
+    [SerializeField]
     private GameObject GameOverScreen;
 
     void OnMove(InputValue value)
@@ -31,12 +40,25 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(_movement.magnitude > 0)
+        if (_movement.magnitude > 0)
         {
-            Body.AddForce((Vector3)_movement * Speed);
-        } else
+            Body.linearVelocity = new Vector3(_movement.x * Speed, Body.linearVelocity.y, _movement.y * Speed);
+        }
+        else
         {
-            Body.linearVelocity *= SpeedDecrease;
+            Body.linearVelocity = new Vector3(
+                Body.linearVelocity.x * SpeedDecrease,
+                Body.linearVelocity.y,
+                Body.linearVelocity.z * SpeedDecrease
+            );
+        }
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame && IsGrounded())
+        {
+            Body.linearVelocity = new Vector3(Body.linearVelocity.x, JumpForce, Body.linearVelocity.z);
         }
     }
 
@@ -54,5 +76,10 @@ public class Player : MonoBehaviour
                 GameOverScreen.SetActive(true);
             }
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, GroundCheckDistance, GroundLayers);
     }
 }
